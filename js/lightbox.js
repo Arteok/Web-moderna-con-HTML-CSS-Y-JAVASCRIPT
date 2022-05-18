674e28d2bf6a125fc065fdcbdd5e32e720b0bccb
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-05-18 18:24:01 -0300

    This is a new commit for what I originally planned to be amended

---
+++ js/lightbox.js
@@ -0,0 +1,18 @@
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+//console.log(imagenes)
+//console.log(imagenesLight)
+//console.log(contenedorLight)
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click',()=>{alert("auch, me dolio")})
+})
+
+
+
+
+
+
+
